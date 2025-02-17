e8a6fb2a1c41a396971b7f769fb32085cbb42f46
Juan11309 <juan.berrio01@usa.edu.co>
2025-02-17 07:39:06 -0500

    Create taller.py
    puntos 1,2 y 3

---
+++ primer_punto.py
@@ -0,0 +1,57 @@
+
+
+#punto 1
+
+# Suma
+suma_1 = int(input("Ingresa el primer numero: "))
+suma_2 = int(input("Ingresa el segundo numero: "))
+
+print("el resultado de la suma es: ", suma_1 + suma_2)
+
+# Resta 
+resta_1 = int(input("Ingresa el primer numero: "))
+resta_2 = int(input("Ingresa el segundo numero: "))
+
+print("el resultado de la resta es: ", resta_1 - resta_2)
+
+# Multiplicacion
+digito_1 = int(input("Ingresa el primer numero: "))
+digito_2 = int(input("Ingresa el segundo numero: "))
+
+print("El resultado de la multiplicacion es: ", digito_1 * digito_2)
+
+# Division
+num_1 = int(input("Ingresa el primer numero: "))
+num_2 = int(input("Ingresa el segundo numero: "))
+
+print(" el resultado de la división es: ", num_1 / num_2)
+
+# Modulo
+numero_1 = int(input("Ingresa el primer numero: "))
+numero_2 = int(input("Ingresa el segundo numero: "))
+
+print("El resultado del modulo es: ", numero_1 / numero_2)
+
+
+#punto 2
+producto = int(input("ingrese el valor del producto sin el descuento: $"))
+ahorro = int((producto * 0.15))
+precio_con_descuento = int (producto - ahorro)
+print(f"el ahorro es de:{ahorro} y el total a pagar son ^{precio_con_descuento}")
+
+
+#punto 3
+
+numero = 10
+
+suma = numero + 5
+print = (suma)
+
+resta = suma - 3
+print = (resta)
+
+multiplicacion = resta * 2
+print = (multiplicacion)
+
+division = multiplicacion / 4
+print = (division)
